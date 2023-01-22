bf15efa3aad19fa5d1c172f90568f83b744fb8a5
Thomas Carli <thomascarli@gmail.com>
2023-01-22 17:35:59 +0100

    v1

--- src/ConsistentHashing/ConsistentHashing.test.ts
+++ src/ConsistentHashing/ConsistentHashing.test.ts
@@ -1,5 +1,8 @@
 import _ from "lodash";
-import { consistentHashing } from "./ConsistentHashing";
+import {
+  consistentHashing,
+  consistentHashingFirebaseArray,
+} from "./ConsistentHashing";
 
 const randomString = () => {
   return Math.random().toString(36).substring(7);
@@ -7,32 +10,60 @@ const randomString = () => {
 
 describe("Firebase Consistent Hashing", () => {
   it("should return an empty array when only one server", async () => {
-    const res = consistentHashing("anyRessource", 0);
+    const res = consistentHashingFirebaseArray("anyRessource", 0);
     expect(res).toEqual([]);
+  });
+
+  it("should be fixed", async () => {
+    const res = consistentHashingFirebaseArray("anyRessource", 10);
+    expect(res).toEqual([
+      "1-5",
+      "2-5",
+      "3-28",
+      "4-35",
+      "5-35",
+      "6-35",
+      "7-60",
+      "8-60",
+      "9-60",
+      "10-60",
+    ]);
   });
 
   it("should be deterministic", async () => {
     const s = randomString();
-    console.log(_.times(10, (i) => consistentHashing(`string-${i}`, 5)));
-    expect(consistentHashing(s, 10)).toEqual(consistentHashing(s, 10));
+    // console.log(_.times(10, (i) => consistentHashing(`string-${i}`, 10)));
+    expect(consistentHashingFirebaseArray(s, 10)).toEqual(
+      consistentHashingFirebaseArray(s, 10)
+    );
   });
 
   it.skip("should distribute evenly accross 10 servers", async () => {
     const randomStrings = _.times(100, () => randomString());
     const maxNumberOfServers = 10;
     const res = randomStrings.map((s) =>
-      consistentHashing(s, maxNumberOfServers)
+      consistentHashingFirebaseArray(s, maxNumberOfServers)
     );
 
     for (
-      let serverCount = 0;
+      let serverCount = 1;
       serverCount <= maxNumberOfServers;
       serverCount++
     ) {
       const counts = _.countBy(
-        res.map((r) => r[serverCount]).map((s) => s.split("-")[1])
+        res.map((r) => r[serverCount - 1]).map((s) => s.split("-")[1])
       );
-      console.log("numServers = " + serverCount, counts);
+      console.log("numServers = " + serverCount * 10, counts);
     }
+  });
+
+  it.skip("should distribute evenly 1000 items accross 100 servers", async () => {
+    const randomStrings = _.times(1000, () => randomString());
+    const numServers = 10 * 10;
+    const cHash = consistentHashing(numServers);
+    const res = randomStrings.map((s) => cHash(s));
+
+    const counts = _.countBy(res);
+    console.log(counts);
   });
 });
--- src/ConsistentHashing/ConsistentHashing.ts
+++ src/ConsistentHashing/ConsistentHashing.ts
@@ -2,16 +2,25 @@
 import ConsistentHash from "consistent-hashing";
 import _ from "lodash";
 
-export const consistentHashing = (
+export const consistentHashingFirebaseArray = (
   resourceName: string | number,
   maxNumberOfServers: number
 ): string[] => {
   const firebaseArray = [];
+  const hr = new ConsistentHash([]);
+
   for (let serverCount = 1; serverCount <= maxNumberOfServers; serverCount++) {
-    const hr = new ConsistentHash(_.times(10 * serverCount, (i) => `${i}`));
+    _.times(10, (i) => hr.addNode(String((serverCount - 1) * 10 + i)));
     const serverToUse = hr.getNode(String(resourceName));
-    firebaseArray.push(serverToUse);
+    firebaseArray.push(`${serverCount}-${serverToUse}`);
   }
 
   return firebaseArray;
+};
+
+export const consistentHashing = (serverCount: number) => {
+  const hr = new ConsistentHash(_.times(serverCount, (i) => String(i)));
+  return (resourceName: string | number) => {
+    return Number(hr.getNode(String(resourceName)));
+  };
 };
