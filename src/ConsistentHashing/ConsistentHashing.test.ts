f518e2403b96564da8a8aee9a96cbf0244df3b1b
Thomas Carli <thomascarli@gmail.com>
2023-01-22 18:52:29 +0100

    getShardsToListenTo()

--- src/ConsistentHashing/ConsistentHashing.test.ts
+++ src/ConsistentHashing/ConsistentHashing.test.ts
@@ -2,6 +2,7 @@ import _ from "lodash";
 import {
   consistentHashing,
   consistentHashingFirebaseArray,
+  getShardsToListenTo,
 } from "./ConsistentHashing";
 
 const randomString = () => {
@@ -38,6 +39,15 @@ describe("Firebase Consistent Hashing", () => {
     );
   });
 
+  it("array and single method must match", async () => {
+    const s = randomString();
+    expect(consistentHashingFirebaseArray(s, 10)).toEqual(
+      _.times(10, (i) => consistentHashing((i + 1) * 10)(s)).map(
+        (s, i) => `${i + 1}-${s}`
+      )
+    );
+  });
+
   it.skip("should distribute evenly accross 10 servers", async () => {
     const randomStrings = _.times(100, () => randomString());
     const maxNumberOfServers = 10;
@@ -65,5 +75,106 @@ describe("Firebase Consistent Hashing", () => {
 
     const counts = _.countBy(res);
     console.log(counts);
+  });
+});
+
+describe("getShardsToListenTo", () => {
+  it("should return all shards when there is only 1 server", async () => {
+    const res = getShardsToListenTo(0, 1);
+    expect(res).toEqual(null);
+  });
+  it("should return items for server i=0 (total 2)", async () => {
+    const res = getShardsToListenTo(0, 2);
+    expect(res).toEqual([
+      "2-0",
+      "2-1",
+      "2-2",
+      "2-3",
+      "2-4",
+      "2-5",
+      "2-6",
+      "2-7",
+      "2-8",
+      "2-9",
+    ]);
+  });
+  it("should return items for server i=3 (total 7)", async () => {
+    const res = getShardsToListenTo(3, 7);
+    expect(res).toEqual([
+      "7-30",
+      "7-31",
+      "7-32",
+      "7-33",
+      "7-34",
+      "7-35",
+      "7-36",
+      "7-37",
+      "7-38",
+      "7-39",
+    ]);
+  });
+  it("should return items for server i=10 (total 11)", async () => {
+    const res = getShardsToListenTo(10, 11);
+    expect(res).toEqual([
+      "11-100",
+      "11-101",
+      "11-102",
+      "11-103",
+      "11-104",
+      "11-105",
+      "11-106",
+      "11-107",
+      "11-108",
+      "11-109",
+    ]);
+  });
+  it("should return items for server i=11 (total 12)", async () => {
+    const res = getShardsToListenTo(11, 12);
+    expect(res).toEqual([
+      "11-29",
+      "11-39",
+      "11-49",
+      "11-59",
+      "11-69",
+      "11-79",
+      "11-89",
+      "11-99",
+      "11-109",
+    ]);
+  });
+  it("should return items for server i=10 (total 12)", async () => {
+    const res = getShardsToListenTo(10, 12);
+    expect(res).toEqual([
+      "11-100",
+      "11-101",
+      "11-102",
+      "11-103",
+      "11-104",
+      "11-105",
+      "11-106",
+      "11-107",
+      "11-108",
+    ]);
+  });
+  it("should return items for server i=12 (total 13)", async () => {
+    const res = getShardsToListenTo(12, 13);
+    expect(res).toEqual([
+      "11-9",
+      "11-19",
+      "11-68",
+      "11-78",
+      "11-88",
+      "11-98",
+      "11-108",
+      "11-109",
+    ]);
+  });
+  it("should return items for server i=0 (total 110)", async () => {
+    const res = getShardsToListenTo(0, 110);
+    expect(res).toEqual(["11-0"]);
+  });
+  it("should return items for server i=0 (total 110)", async () => {
+    const res = getShardsToListenTo(1, 110);
+    expect(res).toEqual(["11-10"]);
   });
 });
--- src/ConsistentHashing/ConsistentHashing.ts
+++ src/ConsistentHashing/ConsistentHashing.ts
@@ -4,12 +4,12 @@ import _ from "lodash";
 
 export const consistentHashingFirebaseArray = (
   resourceName: string | number,
-  maxNumberOfServers: number
+  tensOfServerCount: number
 ): string[] => {
   const firebaseArray = [];
   const hr = new ConsistentHash([]);
 
-  for (let serverCount = 1; serverCount <= maxNumberOfServers; serverCount++) {
+  for (let serverCount = 1; serverCount <= tensOfServerCount; serverCount++) {
     _.times(10, (i) => hr.addNode(String((serverCount - 1) * 10 + i)));
     const serverToUse = hr.getNode(String(resourceName));
     firebaseArray.push(`${serverCount}-${serverToUse}`);
@@ -24,3 +24,44 @@ export const consistentHashing = (serverCount: number) => {
     return Number(hr.getNode(String(resourceName)));
   };
 };
+
+export const getShardsToListenTo = (
+  serverIndex: number,
+  serverCount: number,
+  maxArrayInQuery: number = 10 // Default Firebase value
+) => {
+  // For serverCount between 2 and 11, it's easy.
+  if (serverCount === 1) {
+    return null;
+  }
+  if (serverCount <= 11) {
+    return _.times(
+      maxArrayInQuery,
+      (i) => `${serverCount}-${serverIndex * maxArrayInQuery + i}`
+    );
+  }
+
+  // Build the whole line for serverCount > 11
+  const matrix = _.times(11, (y) =>
+    _.times(maxArrayInQuery, (x) => x + y * 10)
+  );
+
+  let k = 10;
+  for (let i = 12; i <= serverCount; i++) {
+    let newLine = [];
+
+    const numItemsToTake = Math.floor((11 * maxArrayInQuery) / i);
+
+    for (let j = 0; j < numItemsToTake; j++) {
+      newLine.push(matrix[k].pop());
+      k = modulo(k - 1, i - 1);
+    }
+    matrix.push(newLine.sort((a, b) => a - b));
+  }
+
+  return matrix[serverIndex].map((i) => `11-${i}`);
+};
+
+// 3 servers => 3-20 => 3-29
+
+const modulo = (n: number, m: number) => ((n % m) + m) % m;
