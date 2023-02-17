5873aa34ede7154a311f825b7f02d70d06af2f29
Thomas Carli <thomascarli@gmail.com>
2023-02-17 16:09:06 +0100

    cleanup

--- website/app/routes/login.tsx
+++ website/app/routes/login.tsx
@@ -1,12 +1,5 @@
-import type { LoaderArgs, MetaFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node";
 import { SignIn } from "~/components/login/SignIn";
-
-export async function loader({ request }: LoaderArgs) {
-  // const userId = await getUserId(request);
-  // if (userId) return redirect("/");
-  return json({});
-}
 
 export const meta: MetaFunction = () => {
   return {
