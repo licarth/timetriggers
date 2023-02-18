9b469d6064fd0c87cc7524e617d9d29c3fb6b456
Thomas Carli <thomascarli@gmail.com>
2023-02-18 12:40:04 +0100

    module augmentation

---
+++ agent/src/index.d.ts
@@ -0,0 +1,9 @@
+import type { DecodeError } from "io-ts/lib/Decoder";
+import * as E from "fp-ts/lib/Either.js";
+import * as K from "io-ts/lib/Kleisli.js";
+
+declare module "io-ts/lib/Decoder.js" {
+  export interface Decoder<I, A> extends K.Kleisli<E.URI, I, DecodeError, A> {
+    decode: (i: I) => E.Either<DecodeError, A>;
+  }
+}
