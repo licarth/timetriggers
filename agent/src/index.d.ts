244a73a92964439d9b140496b68e525d0a2aebca
Thomas Carli <thomascarli@gmail.com>
2023-02-18 12:45:07 +0100

    add comment

--- agent/src/index.d.ts
+++ agent/src/index.d.ts
@@ -2,6 +2,7 @@ import type { DecodeError } from "io-ts/lib/Decoder";
 import * as E from "fp-ts/lib/Either.js";
 import * as K from "io-ts/lib/Kleisli.js";
 
+// See https://github.com/gcanti/io-ts/issues/644
 declare module "io-ts/lib/Decoder.js" {
   export interface Decoder<I, A> extends K.Kleisli<E.URI, I, DecodeError, A> {
     decode: (i: I) => E.Either<DecodeError, A>;
