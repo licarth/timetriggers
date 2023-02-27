0b4312834385095172c8f3b2794636e3da322552
Thomas Carli <thomascarli@gmail.com>
2023-02-27 18:38:09 +0100

    16 chars in JobId

--- domain/src/domain/JobId.ts
+++ domain/src/domain/JobId.ts
@@ -4,12 +4,18 @@ import * as Codec from "io-ts/lib/Codec.js";
 export namespace JobId {
   export const codec = stringOpaqueCodec("JobId");
   export const factory = (): JobId => {
-    return `${randomString(32)}` as JobId;
+    return `${randomString(16)}` as JobId;
   };
 }
 
-const randomString = (length: number): string => {
-  return Math.random().toString(36).substr(2, length);
+const randomString = (length: number = 16): string => {
+  // Max length: 36
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+  let result = "";
+  for (let i = length; i > 0; --i) {
+    result += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return result;
 };
 
 export type JobId = Codec.TypeOf<typeof JobId.codec>;
