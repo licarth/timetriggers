a0cebe91b5d5cbeeabfebcf40f44f24ca008c993
Thomas Carli <thomascarli@gmail.com>
2023-03-06 15:06:21 +0100

    use API instead of kubectl

--- agent/src/Coordination/KubernetesStatefulsetCoordinationClient.ts
+++ agent/src/Coordination/KubernetesStatefulsetCoordinationClient.ts
@@ -1,5 +1,5 @@
-import { exec } from "child_process";
 import * as TE from "fp-ts/lib/TaskEither.js";
+import * as k8s from "@kubernetes/client-node";
 import _ from "lodash";
 import { Observable, ReplaySubject } from "rxjs";
 import {
@@ -11,12 +11,19 @@ export class KubernetesStatefulsetCoordinationClient
   implements CoordinationClient
 {
   private currentClusterSize?: number;
+  private k8sApi;
   private currentNodeId: number;
   private clearIntervalId?: NodeJS.Timeout;
   private subject = new ReplaySubject<ClusterNodeInformation>(1); // Emit the most recent information to new subscribers
 
   private constructor({ currentNodeId }: { currentNodeId: number }) {
     this.currentNodeId = currentNodeId;
+
+    const kc = new k8s.KubeConfig();
+    kc.loadFromDefault();
+
+    const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
+    this.k8sApi = k8sApi;
   }
 
   private async startClusterSizePolling() {
@@ -71,24 +78,16 @@ export class KubernetesStatefulsetCoordinationClient
    */
   private async getClusterSize() {
     try {
-      const availableReplicas = parseInt(
-        (
-          await promiseExec(
-            "kubectl get statefulset timetriggers-agent -o jsonpath='{.status.availableReplicas}'"
-          )
-        )
-          .toString()
-          .trim()
+      const ss = await this.k8sApi.readNamespacedStatefulSetStatus(
+        "timetriggers-agent",
+        "doi-production"
       );
-      const replicas = parseInt(
-        (
-          await promiseExec(
-            "kubectl get statefulset timetriggers-agent -o jsonpath='{.spec.replicas}'"
-          )
-        )
-          .toString()
-          .trim()
-      );
+      const availableReplicas = ss.body.status?.availableReplicas;
+      const replicas = ss.body.spec?.replicas;
+
+      if (availableReplicas === undefined || replicas === undefined) {
+        throw new Error("Could not get acailable replicas or replicas");
+      }
 
       return Math.min(availableReplicas, replicas);
     } catch (e) {
@@ -97,14 +96,3 @@ export class KubernetesStatefulsetCoordinationClient
     }
   }
 }
-
-const promiseExec = (command: string) =>
-  new Promise<string>((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(stdout);
-      }
-    });
-  });
