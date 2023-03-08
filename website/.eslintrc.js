fb5a876a0763f275932cf48b0d187146ca70ab7e
Thomas Carli <thomascarli@gmail.com>
2023-03-08 14:41:28 +0100

    fix eslint and prettier

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -1,24 +1,25 @@
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
-  extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
-  ],
-  env: {
-    "cypress/globals": true,
-  },
-  plugins: ["cypress"],
-  // We're using vitest which has a very similar API to jest
-  // (so the linting plugins work nicely), but we have to
-  // set the jest version explicitly.
-  settings: {
-    jest: {
-      version: 28,
+    extends: [
+        '@remix-run/eslint-config',
+        '@remix-run/eslint-config/jest',
+        'plugin:testing-library/react',
+        'plugin:jest-dom/recommended',
+        'plugin:prettier/recommended',
+    ],
+    env: {
+        'cypress/globals': true,
     },
-  },
-  rules: {
-    "react-hooks/exhaustive-deps": "off",
-  },
+    plugins: ['cypress', 'testing-library', 'jest-dom', 'prettier'],
+    // We're using vitest which has a very similar API to jest
+    // (so the linting plugins work nicely), but we have to
+    // set the jest version explicitly.
+    settings: {
+        jest: {
+            version: 28,
+        },
+    },
+    rules: {
+        'react-hooks/exhaustive-deps': 'off',
+    },
 };
