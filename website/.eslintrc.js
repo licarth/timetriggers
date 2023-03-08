811a42b4fe0883b1e6210a5cf24d186338fab404
Thomas Carli <thomascarli@gmail.com>
2023-03-08 15:04:44 +0100

    eslint config

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -1,25 +1,26 @@
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
-    extends: [
-        '@remix-run/eslint-config',
-        '@remix-run/eslint-config/jest',
-        'plugin:testing-library/react',
-        'plugin:jest-dom/recommended',
-        'plugin:prettier/recommended',
-    ],
-    env: {
-        'cypress/globals': true,
+  extends: [
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/jest',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    'cypress/globals': true,
+  },
+  plugins: ['cypress', 'testing-library', 'jest-dom', 'prettier'],
+  // We're using vitest which has a very similar API to jest
+  // (so the linting plugins work nicely), but we have to
+  // set the jest version explicitly.
+  settings: {
+    jest: {
+      version: 28,
     },
-    plugins: ['cypress', 'testing-library', 'jest-dom', 'prettier'],
-    // We're using vitest which has a very similar API to jest
-    // (so the linting plugins work nicely), but we have to
-    // set the jest version explicitly.
-    settings: {
-        jest: {
-            version: 28,
-        },
-    },
-    rules: {
-        'react-hooks/exhaustive-deps': 'off',
-    },
+  },
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    'prettier/prettier': 'warn',
+  },
 };
